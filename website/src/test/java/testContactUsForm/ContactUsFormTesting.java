33be6b9867e2351056b6a90e4f9dd3f60b465993
“QhAdmin” <qh.serbia@gmail.com>
2016-12-20 13:26:37 +0100

    Proba za commit

--- website/src/test/java/testContactUsForm/ContactUsFormTesting.java
+++ website/src/test/java/testContactUsForm/ContactUsFormTesting.java
@@ -97,7 +97,7 @@ public class ContactUsFormTesting {
 //		assertEquals(contactUsPage.getNameError().getText(), "Please enter valid characters.");
 //		Utils.takeSnapShot(driver, "test.png");
 //
-// 		Milorad comment
+// 		Resenje za brisanje
 //	}
 
 	@AfterMethod
