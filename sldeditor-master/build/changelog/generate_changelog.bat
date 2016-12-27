e2b9a6ff135042c62a9537bf839a88d0d61fb775
QhAdmin <qh.serbia@gmail.com>
2016-12-27 11:09:30 +0100

    Java_app

--- sldeditor-master/build/changelog/generate_changelog.bat
+++
@@ -1,2 +0,0 @@
-rem Assumes CHANGELOG_GITHUB_TOKEN environment variable has been set
-github_changelog_generator -o ..\..\CHANGELOG.md
--- sldeditor-master/build/changelog/generate_changelog.sh
+++
@@ -1,2 +0,0 @@
-# Assumes CHANGELOG_GITHUB_TOKEN environment variable has been set
-github_changelog_generator -o ../../CHANGELOG.md
